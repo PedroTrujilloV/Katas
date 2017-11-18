6ded1c610efdca5456051d070f2e021ddeefa5c7
Pedro Trujillo V <ptrujillo@crunchy.co>
2017-11-18 12:33:11 -0500

    Added Main StoryBoard components and its layout

---
+++ Apps/App_2/App_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App_2
+//
+//  Created by Pedro Trujillo on 11/17/17.
+//  Copyright © 2017 Pedro Trujillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Apps/App_2/App_2/MainUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainUIViewController.h
+//  App_2
+//
+//  Created by Pedro Trujillo on 11/17/17.
+//  Copyright © 2017 Pedro Trujillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainUIViewController : UIViewController
+
+@end
